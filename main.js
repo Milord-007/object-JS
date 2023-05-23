02efa47481626b7e728890c4b4a9400c88987496
Ardasher <misterardasher@gmail.com>
2023-05-23 11:14:29 +0500

    object

---
+++ main.js
@@ -0,0 +1,13 @@
+let person = {
+    firstName: 'Ardasher',
+    lastName: 'Bobokalonov',
+    age: 30,
+    address: {
+      city: 'New York',
+      state: 'NY'
+    },
+    hobbies: ['reading', 'sports']
+  };
+  console.log(person.firstName); // "Ardasher"
+  console.log(person.address.city); // "New York"
+  console.log(person.hobbies[1]); // "sports"
